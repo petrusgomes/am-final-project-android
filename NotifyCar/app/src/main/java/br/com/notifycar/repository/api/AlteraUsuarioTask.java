270af56cbc8914c1fa53842a410e09231172085f
Douglas Carvalho <douglascarvalhoromano@gmail.com>
2016-09-27 19:52:08 -0300

    notification push fcm, layout mapa, nova task

---
+++ NotifyCar/app/src/main/java/br/com/notifycar/repository/api/AlteraUsuarioTask.java
@@ -0,0 +1,36 @@
+package br.com.notifycar.repository.api;
+
+import android.os.AsyncTask;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import br.com.notifycar.util.UtilJson;
+
+/**
+ * Created by Desenvolvimento on 27/09/2016.
+ */
+public class AlteraUsuarioTask extends AsyncTask<String, Void, String> {
+
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        URL url = null;
+        try {
+            url = new URL("http://notifycar-api.mybluemix.net/gateway/register/494101363422774");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("PUT");
+
+
+            if (conn.getResponseCode() == 200) {
+
+            }
+        }catch (Exception e){
+            e.getMessage();
+        }
+        return null;
+    }
+
+}
--- NotifyCar/app/src/main/java/br/com/notifycar/service/MyFirebaseMessagingService.java
+++ NotifyCar/app/src/main/java/br/com/notifycar/service/MyFirebaseMessagingService.java
@@ -1,10 +1,24 @@
 package br.com.notifycar.service;
 
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import br.com.notifycar.R;
+import br.com.notifycar.controller.mapa.MapsActivity;
+import br.com.notifycar.menu.MenuTabActivity;
+import br.com.notifycar.menu.fragments.HomeFragment;
 
 /**
  * Created by Desenvolvimento on 22/09/2016.
@@ -16,9 +30,42 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // TODO: Handle FCM messages here.
         // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
+        // Also if you intend on generating your own notifications as a result of a received FCMti
         // message, here is where that should be initiated.
+
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+
+        sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+
     }
+
+
+    private void sendNotification(String title, String messageBody) {
+        Intent intent = new Intent(this, MenuTabActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setColor(getResources().getColor(R.color.colorAccent))
+                .setSmallIcon(R.drawable.icon_mapcar)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentTitle(title)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+
+
 }
